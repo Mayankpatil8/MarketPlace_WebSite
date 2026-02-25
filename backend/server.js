1d8361b05b7578dd6540efa49320f537374807cc
Mayank Patil <your-email@gmail.com>
2026-02-25 14:55:50 +0530

    Fix it

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://marketplacemayank.netlify.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
